2b25ea70d98dd798d2b62a84afa95614f1aea049
liuzg <liuzg50505@qq.com>
2019-07-28 17:27:24 +0800

    mytreeeditor api
    Signed-off-by: liuzg <liuzg50505@qq.com>

--- src/main/java/com/liuzg/def/MyTreeEditor.java
+++ src/main/java/com/liuzg/def/MyTreeEditor.java
@@ -17,9 +17,11 @@ public class MyTreeEditor extends VBox {
     private List<Consumer<MyTreeNode>> selectionChangedHandlers;
 
     private InstanceNodePool pool;
+    private List<MyTreeNode> currentnodes;
 
 
     public MyTreeEditor(Instance instance) {
+        currentnodes = new ArrayList<>();
         selectionChangedHandlers = new ArrayList<>();
         rootInstance = instance;
         pool = new InstanceNodePool();
@@ -28,19 +30,24 @@ public class MyTreeEditor extends VBox {
 
     }
 
+    private <T> void notifyHandlers(List<Consumer<T>> handlers, T target){
+        for(Consumer<T> handler: handlers){
+            handler.accept(target);
+        }
+    }
+
     public void renderUI() {
         this.getChildren().clear();
 
         MyTreeGenerator generator = new MyTreeGenerator(pool);
         rootnode = generator.generateTree((ConstructorInstance) rootInstance);
         List<MyTreeNode> nodes = generator.generateOrderedVisibleTreeNodes(rootnode);
+        currentnodes = nodes;
         for(MyTreeNode node: nodes) {
             node.addClickedListener(treeNode -> {
                 if(selectedNode!=treeNode) {
                     selectedNode = treeNode;
-                    for(Consumer<MyTreeNode> handler: selectionChangedHandlers){
-                        handler.accept(treeNode);
-                    }
+                    notifyHandlers(selectionChangedHandlers, node);
                     refreshSelected();
                 }
             });
@@ -67,10 +74,10 @@ public class MyTreeEditor extends VBox {
 
     private void refreshSelected() {
 //        Set<MyTreeNode> nodes = pool.getTreeNodes();
-        MyTreeGenerator generator = new MyTreeGenerator(pool);
-        List<MyTreeNode> nodes = generator.generateOrderedVisibleTreeNodes(rootnode);
+//        MyTreeGenerator generator = new MyTreeGenerator(pool);
+//        List<MyTreeNode> nodes = generator.generateOrderedVisibleTreeNodes(rootnode);
 
-        for(MyTreeNode node: nodes) {
+        for(MyTreeNode node: currentnodes) {
             node.unselect();
         }
         if(selectedNode!=null) {
@@ -78,7 +85,7 @@ public class MyTreeEditor extends VBox {
         }
     }
 
-    public void refreshExpand() {
+    private void refreshExpand() {
         this.getChildren().clear();
 
         MyTreeGenerator generator = new MyTreeGenerator(pool);
@@ -105,5 +112,43 @@ public class MyTreeEditor extends VBox {
             refreshSelected();
         }
     }
+
+    public void removeTreeNode(MyTreeNode treeNode) {
+        if(treeNode instanceof MyTreePropertyInstanceNode) {
+            MyTreePropertyInstanceNode current = (MyTreePropertyInstanceNode) treeNode;
+            ConstructorInstance parent = current.getConstructureInstance();
+            String property = current.getProperty();
+            parent.setProperty(property, null);
+            renderUI();
+        }else if(treeNode instanceof MyTreeInstanceNode) {
+            MyTreeInstanceNode current = (MyTreeInstanceNode) treeNode;
+            MyTreePropertyNode propertyNode = (MyTreePropertyNode) current.parentNode;
+            ConstructorInstance parent = propertyNode.getConstructureInstance();
+            String property = propertyNode.getProperty();
+            List value = (List) parent.getProperty(property);
+            value.remove(current.getConstructureInstance());
+            renderUI();
+        }
+    }
+
+    public void addInstance(MyTreeNode treeNode, Instance instance) {
+        if(treeNode instanceof MyTreePropertyInstanceNode) {
+            MyTreePropertyInstanceNode current = (MyTreePropertyInstanceNode) treeNode;
+            ConstructorInstance parent = current.getConstructureInstance();
+            String property = current.getProperty();
+            parent.setProperty(property, instance);
+            renderUI();
+        }else if(treeNode instanceof MyTreeInstanceNode) {
+            MyTreeInstanceNode current = (MyTreeInstanceNode) treeNode;
+            MyTreePropertyNode propertyNode = (MyTreePropertyNode) current.parentNode;
+            ConstructorInstance parent = propertyNode.getConstructureInstance();
+            String property = propertyNode.getProperty();
+            List value = (List) parent.getProperty(property);
+            value.add(instance);
+            renderUI();
+        }
+    }
+
+
 
 }
